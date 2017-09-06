bf6d37b11bcea2715b1a6aa445bb35c286058934
evuz <jesus_jgb_45@hotmail.com>
2017-09-06 14:19:32 +0200

    Initialize firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase';
 
 import { SigninPage } from '../pages/signin/signin';
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class MyApp implements OnInit {
   rootPage = SigninPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCizpJfAuIZ8Y7QoVx15zvWa5Q7bE6OEAM",
+      authDomain: "followmycrypto.firebaseapp.com",
+      databaseURL: "https://followmycrypto.firebaseio.com",
+      projectId: "followmycrypto",
+      storageBucket: "followmycrypto.appspot.com",
+      messagingSenderId: "613438993917"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  ngOnInit() {
+
   }
 }
 
