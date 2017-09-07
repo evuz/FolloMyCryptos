7fbb66590a7932dcdd42bde97c40504f6a0b2c03
evuz <jesus_jgb_45@hotmail.com>
2017-09-07 17:23:48 +0200

    Show total portfolio

--- src/pages/portfolio/portfolio.ts
+++ src/pages/portfolio/portfolio.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { NewOperationPage } from './../new-operation/new-operation';
+
+import { UserService } from './../../services/user';
+import { CoinMarketService } from './../../services/coinMarket';
+
+import { User } from './../../models/user';
 
 @Component({
   selector: 'page-portfolio',
   templateUrl: 'portfolio.html',
 })
-export class PortfolioPage {
+export class PortfolioPage implements OnInit {
+  user: User;
+  total: { total: number, profit: number };
 
   constructor(
-    public navCtrl: NavController
+    private navCtrl: NavController,
+    private coinMarketService: CoinMarketService,
+    private userService: UserService
   ) { }
 
+  ngOnInit() {
+    this.user = this.userService.getUser();
+    this.coinMarketService.fetchNames();
+    this.getTotalOperations();
+  }
+
+  goToNewOperation() {
+    this.navCtrl.push(NewOperationPage);
+  }
+
+  private getTotalOperations() {
+    const { operations } = this.user;
+    const request = operations.map((operation) => {
+      return new Promise((res, rej) => {
+        this.coinMarketService.getCoinById(operation.currency.id)
+          .subscribe(res)
+      });
+    });
+
+    Promise.all(request)
+      .then((coins: any[]) => {
+        this.total = coins.reduce((acum, coin, index) => {
+          const total = acum.total + operations[index].amount * coin.priceUSD;
+          const profit = acum.profit + total - operations[index].investment;
+          return {
+            total,
+            profit
+          }
+        }, { profit: 0, total: 0 });
+      })
+  }
 }
