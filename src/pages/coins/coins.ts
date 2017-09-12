c8ff729521622620502e45bf5e8a2b14a16371c4
evuz <jesus_jgb_45@hotmail.com>
2017-09-12 10:58:51 +0200

    Show coins value with prefence currency

--- src/pages/coins/coins.ts
+++ src/pages/coins/coins.ts
@@ -1,7 +1,9 @@
+import { SettingsService } from './../../services/settings';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoadingController, Content } from 'ionic-angular';
 
 import { CoinMarketService } from './../../services/coinMarket';
+import { CoinConvertService } from './../../services/coinConvert';
 
 @Component({
   selector: 'page-coins',
@@ -9,14 +11,31 @@ import { CoinMarketService } from './../../services/coinMarket';
 })
 export class CoinsPage implements OnInit {
   @ViewChild(Content) content: Content;
+  moneyValue: string;
   items: any[] = [];
 
   constructor(
     private coinMarketService: CoinMarketService,
+    private settingsService: SettingsService,
+    private coinConvertService: CoinConvertService,
     private loadingCtrl: LoadingController
   ) { }
 
   ngOnInit() {
+    this.moneyValue = this.settingsService.getSettings().valueCurrency;
+    this.settingsService.settingsChanged
+      .subscribe(async (settings) => {
+        if (this.moneyValue != settings.valueCurrency) {
+          this.moneyValue = settings.valueCurrency;
+          const conversion = await this.coinConvertService.getConversion();
+
+          this.items = this.items.map((el) => {
+            const price = this.moneyValue === 'usd' ?
+              el.price / conversion : el.price * conversion;
+            return Object.assign({}, el, { price });
+          });
+        }
+      })
     this.getCoins();
   }
 
@@ -29,16 +48,27 @@ export class CoinsPage implements OnInit {
     this.getCoins(() => refresher.complete());
   }
 
-  private getCoins(cb?: Function) {
+  private async getCoins(cb?: Function) {
     const loading = this.loadingCtrl.create({
       content: 'Please wait...'
     });
     loading.present();
+
+    const conversion = this.moneyValue !== 'usd' ?
+      await this.coinConvertService.getConversion() : undefined;
+
     this.coinMarketService.getCoins()
+      .map((res) => {
+        if (conversion) {
+          return res.map((el) => {
+            return Object.assign({}, el, { price: el.price * conversion });
+          });
+        } else return res;
+      })
       .subscribe((res) => {
         loading.dismiss();
         this.items = res;
-        if(cb) cb();
+        if (cb) cb();
       });
   }
 }
